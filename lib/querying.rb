eef8bb9028cfeb6c2f328dc1bf43603894107b6d
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-11 04:33:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -5,7 +5,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto FROM characters
-  WHERE 
+  WHERE (LENGTH(motto)-LENGTH(REPLACE(motto,’ ‘,’’)))=(SELECT MAX(LENGTH(motto)-LENGTH(REPLACE(motto,’ ‘,’’))) FROM characters);"
   
 
 end
