c86329950f52f11248cfbbe1082982cb71592829
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-11 04:29:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -5,7 +5,8 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters
-  Where (LENGTH(characters.motto) = Max (LENGTH motto)"
+  Where (LENGTH(characters.motto) = DESC (LENGTH motto)
+  LIMIT 1"
 
 end
 
